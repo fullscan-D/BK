2b2508d313a71092e16463a423007b3d9bfe3736
doorumie92 <doorumiee@gmail.com>
2019-10-31 13:22:24 +0900

    main

--- src/main.c
+++ src/main.c
@@ -5,11 +5,19 @@
 #include "saveBookMark.h"
 #include "loadBookMark.h"
 #include "listBookMark.h"
+#include "initBookMark.h"
 
 int main(int args, char * argv[]){
+    
+
+    // command error
     if(errorBookMark(args) == -1){
         printf("Not command\n");
     }
+
+    // .bookmark init
+    initBookMark();
+
     return 0;
 }
 
